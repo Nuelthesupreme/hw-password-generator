293a3339f60c719dcd2abfdf2c93e8a7d833333b
Nuelthesupreme <nuelthesupreme@gmail.com>
2020-04-26 16:20:45 +0100

    moving password generator into new repository

---
+++ script.js
@@ -0,0 +1,51 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+function generatePassword() {
+
+  // prompting for password length 
+  let p_len = 0;
+  while(!p_len || p_len <8 || p_len>128 ){
+      p_len= parseInt(prompt("password length, number between 8 and 128","8"))
+  }
+
+  // prompting for character set
+  const lc_char = "abcdefghijklmnopqrstuvwxyz";
+  const up_char = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const nm_char = "1234567890";
+  const sp_char = "!£$%^&*()_-=+{}.,/";
+  let x ="";
+  if(confirm("add lowercase characters?")){
+    x += lc_char;
+  }
+  if(confirm("add uppercase characters?")){
+    x += up_char;
+  }
+  if(confirm("add numerical characters?")){
+    x += nm_char;
+  }
+  if(confirm("add special characters?")){
+    x += sp_char;
+  }
+
+  // Generating the password
+  let password='';
+  for( var i = 0; i<p_len; i++){
+    var c = Math.floor((Math.random()*x.length)+1);
+    password+= x.charAt(c);
+  }
+  return password;
+}
+
+// Write password to the #password input
+function writePassword() {
+  console.log("Ive been called");
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
